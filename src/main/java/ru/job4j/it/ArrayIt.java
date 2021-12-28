7c98f96d7a92757b92700987b58bd948c9262bbc
Eugene Vovk <eugenevovk@gmail.com>
2021-12-28 13:39:54 +0300

    Что такое итератор. [#4951]

---
+++ src/main/java/ru/job4j/it/ArrayIt.java
@@ -0,0 +1,29 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Сделаем итератор для одномерного массива чисел.
+ */
+public class ArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public ArrayIt(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point++];
+    }
+}
