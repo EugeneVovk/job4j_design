788f5b7b4e34fa60794c29f4f3ad34582fb5fd5a
Eugene Vovk <eugenevovk@gmail.com>
2022-01-07 16:24:59 +0300

    Используя контейнер на базе связанного списка создать контейнер Stack [#71474]

---
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+public class SimpleStack<T> {
+
+    private ForwardLinked<T> linked = new ForwardLinked<>();
+
+    /**
+     * Метод pop() - должен возвращать значение и удалять его из коллекции.
+     */
+    public T pop() {
+        return linked.deleteFirst();
+    }
+
+    /**
+     * Метод push(T value) - помещает значение в коллекцию.
+     */
+    public void push(T value) {
+        linked.addFirst(value);
+    }
+}
