e035d822125808ac87dace0992e3a28500a48850
Eugene Vovk <eugenevovk@gmail.com>
2022-01-14 18:20:05 +0300

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,12 +1,15 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Метод filter должен прочитать файл и вернуть строки,
+ * где предпоследнее значение - это 404.
+ * Метод save должен записывать результат фильтрации в файл.
+ */
 public class LogFilter {
 
     public static List<String> filter(String file) {
@@ -15,17 +18,29 @@ public class LogFilter {
             rsl = in.lines()
                     .filter(n -> n.contains(" 404 "))
                     .collect(Collectors.toList());
+            for (String line : rsl) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return rsl;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)))) {
+            for (String line : log) {
+                out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        String[] lines = log.toString().split(", ");
-        for (String line : lines) {
-            System.out.println(line);
-        }
+        save(log, "404.txt");
     }
 }
