eb124b4119f5323192b26f5ce0300a44444fd2a9
Eugene Vovk <eugenevovk@gmail.com>
2022-01-17 19:21:51 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,11 +11,27 @@ import java.util.function.Predicate;
  * Разработайте программу Search,
  * которая будет искать файлы только по определенному предикату.
  * Программа должна вернуть файлы с расширением js.
+ * <p>
+ * Добавим валидацию.
+ * Валидация - это процесс проверки данных перед выполнением операции.
+ * Блок if - это валидация аргументов.
+ * Если ее не выполнять, то программа будет падать
+ * с ошибками не понятными для пользователя.
+ * Если пользователь забыл указать параметры,
+ * то в консоли он увидит в чем причина и как ее поправить.
+ * Программа должна запускаться с параметрами.
+ * Первый параметр - начальная папка.
+ * Второй параметр - расширение файлов, которые нужно искать.
+ * Необходимо добавить валидацию данных параметров.
  */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js"))
+        if (args.length != 2) {
+            throw new IllegalArgumentException(
+                    "There should be two parameters.");
+        }
+        Path file = Paths.get(args[0]);
+        search(file, p -> p.toFile().getName().endsWith(args[1]))
                 .forEach(System.out::println);
     }
 
