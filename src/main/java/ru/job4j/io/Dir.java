cf02d0e0e4a2af5ce07f672741671a6ec7f84f7c
Eugene Vovk <eugenevovk@gmail.com>
2022-01-17 19:21:20 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -2,9 +2,29 @@ package ru.job4j.io;
 
 import java.io.File;
 
+/**
+ * Эта программа выводит папки проекта. Начальная папка указана сразу в коде.
+ * Изменим программу, чтобы начальная папка передавалась через аргументы запуска.
+ * Добавим валидацию.
+ * Валидация - это процесс проверки данных перед выполнением операции.
+ * Блок if - это валидация аргументов.
+ * Если ее не выполнять, то программа будет падать
+ * с ошибками не понятными для пользователя.
+ * Если пользователь забыл указать параметры,
+ * то в консоли он увидит в чем причина и как ее поправить.
+ * Программа должна запускаться с параметрами.
+ * Первый параметр - начальная папка.
+ * Второй параметр - расширение файлов, которые нужно искать.
+ * Необходимо добавить валидацию данных параметров.
+ */
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(
                     String.format("Not exist %s", file.getAbsoluteFile()));
@@ -14,8 +34,7 @@ public class Dir {
                     String.format("Not directory %s", file.getAbsoluteFile()));
         }
         for (File subfile : file.listFiles()) {
-            System.out.println(
-                    subfile.getName() + " : " + subfile.length());
+            System.out.println(subfile.getName());
         }
     }
 }
