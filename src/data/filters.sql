ce5c758431b94ffa59628a407311d60398b999d5
Eugene Vovk <eugenevovk@gmail.com>
2022-01-24 18:59:55 +0300

    3. Фильтры [#1732 #254765] made queries more readable

--- src/data/filters.sql
+++ src/data/filters.sql
@@ -43,18 +43,56 @@ insert into product(name, type_id, expired_date, price) values
 select * from product;
 
 --1. Написать запрос получение всех продуктов с типом "СЫР"
-select p.name Сыр from product p join type t on type_id =t.id where t.name = 'СЫР';
+select p.name Сыр
+from product p
+join type t
+on type_id =t.id
+where t.name = 'СЫР';
+
 --2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженое"
-select name Мороженое from product where name like '%мороженое%';
+select name Мороженое
+from product
+where name like '%мороженое%';
+
 --3. Написать запрос, который выводит все продукты, срок годности которых уже истек
-select name Продукт, expired_date "Срок годности" from product where expired_date < current_date;
+select name Продукт, expired_date "Срок годности"
+from product
+where expired_date < current_date;
+
 --4. Написать запрос, который выводит самый дорогой продукт.
-select name, price from product where price = (select max(price) from product);
+select name, price
+from product
+where price = (
+    select max(price)
+    from product
+);
+
 --5. Написать запрос, который выводит для каждого типа количество продуктов к нему принадлежащих. В виде имя_типа, количество
-select t.name Тип, count(p.name) "Количество продуктов" from product p join type t on type_id=t.id group by 1;
+select t.name Тип, count(p.name) "Количество продуктов"
+from product p
+join type t
+on type_id=t.id group by 1;
+
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-select p.name from product p join type t on type_id=t.id where t.name = 'СЫР' or t.name = 'МОЛОКО'  order by t.name;
+select p.name
+from product p
+join type t
+on type_id=t.id
+where t.name = 'СЫР'
+or t.name = 'МОЛОКО'
+order by t.name;
+
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук. Под количеством подразумевается количество продуктов определенного типа. Например, если есть тип "СЫР" и продукты "Сыр плавленный" и "Сыр моцарелла", которые ему принадлежат, то количество продуктов типа "СЫР" будет 2. 
-select t.name, count(p.name) from product p join type t on type_id = t.id group by t.name having count(p.name) < 10;
+select t.name, count(p.name)
+from product p
+join type t
+on type_id = t.id
+group by t.name
+having count(p.name) < 10;
+
 --8. Вывести все продукты и их тип.
-select p.name Продукты, t.name Тип from product p join type t on type_id=t.id order by 2;
+select p.name Продукты, t.name Тип
+from product p
+join type t
+on type_id=t.id
+order by 2;
